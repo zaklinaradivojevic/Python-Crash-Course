4b56bb11ec2727124c623ccdb18ece3b6cf6b14b
Zaklina Radivojevic <zaklinaradivojevic77777777@gmail.com>
2023-02-10 11:24:39 +0100

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Python crach course
-# Programming with Mosh
+
 #import sys
 #print(sys.version)
 #print(sys.executable)
---
+++ count_words.py
@@ -0,0 +1,23 @@
+
+name=input('Enter file:')
+handle=open(name)
+
+# Create an empty dictionary
+counts=dict()
+for line in handle:
+    words=line.split()
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+        
+        
+bigcount=None 
+bigword=None 
+count=word.count
+for word.count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount=word 
+        bigcount=count
+        
+        
+print(bigword, bigcount)        
+
